72996b925084c32e6ca7b8892b8769ae8684dfd1
lyaaz <45838887+lyaaz@users.noreply.github.com>
2023-10-14 10:39:15 +0800

    feat: allow hide launcher icon

--- app/src/main/java/org/baiyu/fuckshare/SettingsActivity.java
+++ app/src/main/java/org/baiyu/fuckshare/SettingsActivity.java
@@ -1,15 +1,19 @@
 package org.baiyu.fuckshare;
 
 import android.annotation.SuppressLint;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreferenceCompat;
 
 public class SettingsActivity extends AppCompatActivity {
-
+    private static SharedPreferences prefs;
     /**
      * @noinspection deprecation
      */
@@ -19,9 +23,9 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_layout);
         try {
-            getSharedPreferences(BuildConfig.APPLICATION_ID + "_preferences", MODE_WORLD_READABLE);
+            prefs = getSharedPreferences(BuildConfig.APPLICATION_ID + "_preferences", MODE_WORLD_READABLE);
         } catch (Exception e) {
-            Log.e("fuckshare", e.toString());
+            prefs = getSharedPreferences(BuildConfig.APPLICATION_ID + "_preferences", MODE_PRIVATE);
         }
         getSupportFragmentManager()
                 .beginTransaction()
@@ -30,9 +34,42 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     public static class MySettingsFragment extends PreferenceFragmentCompat {
+        private static final String PREF_KEEP_LAUNCHER_ICON = "keep_launcher_icon";
+        private static final String LAUNCHER_ACTIVITY_NAME = BuildConfig.APPLICATION_ID + ".LauncherActivity";
         @Override
         public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
             setPreferencesFromResource(R.xml.preferences, rootKey);
+
+            SwitchPreferenceCompat keepLauncherIconPreference = findPreference(PREF_KEEP_LAUNCHER_ICON);
+            assert keepLauncherIconPreference != null;
+
+            updateLauncherActivityStatus();
+            keepLauncherIconPreference.setOnPreferenceChangeListener((preference, newValue) -> {
+                enableDisableLauncherIcon((Boolean) newValue);
+                return true;
+            });
+        }
+
+        private void updateLauncherActivityStatus() {
+            Context context = getContext();
+            assert context != null;
+            PackageManager pm = context.getApplicationContext().getPackageManager();
+            ComponentName cn = new ComponentName(BuildConfig.APPLICATION_ID, LAUNCHER_ACTIVITY_NAME);
+            boolean status = pm.getComponentEnabledSetting(cn) != PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean(PREF_KEEP_LAUNCHER_ICON, status);
+            editor.apply();
+        }
+
+        private void enableDisableLauncherIcon(boolean enable) {
+            Context context = getContext();
+            assert context != null;
+            PackageManager pm = context.getApplicationContext().getPackageManager();
+            ComponentName cn = new ComponentName(BuildConfig.APPLICATION_ID, LAUNCHER_ACTIVITY_NAME);
+            int status = enable ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED : PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+            pm.setComponentEnabledSetting(cn, status, PackageManager.DONT_KILL_APP);
+            updateLauncherActivityStatus();
         }
     }
 
