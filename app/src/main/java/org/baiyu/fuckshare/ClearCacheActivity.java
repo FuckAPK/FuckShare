e58b3bc9f2c570c8a22c72bc8e31e13202944de3
lyaaz <45838887+lyaaz@users.noreply.github.com>
2023-10-15 09:19:00 +0800

    add easter eggs

--- app/src/main/java/org/baiyu/fuckshare/ClearCacheActivity.java
+++ app/src/main/java/org/baiyu/fuckshare/ClearCacheActivity.java
@@ -6,17 +6,25 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
+import java.io.File;
+
 public class ClearCacheActivity extends Activity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // clear all cache
-        boolean status = Utils.clearCache(this, 0);
         String message;
-        if (status) {
-            message = getResources().getString(R.string.clear_cache_success);
+
+        File[] files = getCacheDir().listFiles();
+        if (files == null || files.length == 0) {
+            message = getResources().getString(R.string.clear_cache_empty);
         } else {
-            message = getResources().getString(R.string.clear_cache_failed);
+            // clear all cache
+            boolean status = Utils.clearCache(this, 0);
+            if (status) {
+                message = getResources().getString(R.string.clear_cache_success);
+            } else {
+                message = getResources().getString(R.string.clear_cache_failed);
+            }
         }
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
         finish();
