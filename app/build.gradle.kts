7976819c9531d2f1baf89f72b6d7876bc668684f
lyaaz <45838887+lyaaz@users.noreply.github.com>
2023-10-20 19:40:15 +0800

    migrate to kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+android {
+    namespace = "org.baiyu.fuckshare"
+    compileSdk = 34
+    defaultConfig {
+        applicationId = "org.baiyu.fuckshare"
+        minSdk = 30
+        targetSdk = 34
+        versionCode = 31
+        versionName = "5.2"
+        resourceConfigurations += setOf("en", "zh-rCN")
+        vectorDrawables.useSupportLibrary = true
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        debug {
+            applicationIdSuffix = ".debug"
+
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        buildConfig = true
+    }
+}
+dependencies {
+    implementation("androidx.exifinterface:exifinterface:1.3.6")
+    implementation("androidx.preference:preference-ktx:1.2.1")
+    implementation("androidx.work:work-runtime-ktx:2.8.1")
+    implementation("com.jakewharton.timber:timber:5.0.1")
+    implementation("com.geyifeng.immersionbar:immersionbar:3.2.2")
+    implementation("com.google.android.material:material:1.10.0")
+    compileOnly("de.robv.android.xposed:api:82")
+}
