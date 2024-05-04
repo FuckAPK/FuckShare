17330c2c7be459450b525519a44099deccef494c
lyaaz <45838887+lyaaz@users.noreply.github.com>
2024-05-04 21:11:00 +0800

    allow predictive back gesture

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,9 +11,9 @@ android {
         applicationId = "org.baiyu.fuckshare"
         minSdk = 30
         targetSdk = 34
-        versionCode = 61
+        versionCode = 62
 
-        versionName = "8.0"
+        versionName = "8.1"
         resourceConfigurations += setOf("en", "zh-rCN")
         vectorDrawables.useSupportLibrary = true
     }
