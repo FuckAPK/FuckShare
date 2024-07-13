4a02e4de953179a89e5e4c9d43202a8cf0263cd1
lyaaz <45838887+lyaaz@users.noreply.github.com>
2024-07-13 14:19:15 +0800

    Chore: bump app version to v1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         targetSdk = 34
         versionCode = 1
 
-        versionName = "1.0"
+        versionName = "1.1"
         resourceConfigurations += setOf("en", "zh-rCN")
         vectorDrawables.useSupportLibrary = true
     }
