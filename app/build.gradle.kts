8937ed9c7d1859136d4727659a19b5f2ef01558b
lyaaz <45838887+lyaaz@users.noreply.github.com>
2024-11-17 12:08:50 +0800

    chore: update gradle config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,9 +78,6 @@ android {
         buildConfig = true
     }
 }
-composeCompiler {
-    enableStrongSkippingMode = true
-}
 dependencies {
     implementation("androidx.exifinterface:exifinterface:1.3.7")
     implementation("androidx.preference:preference-ktx:1.2.1")
